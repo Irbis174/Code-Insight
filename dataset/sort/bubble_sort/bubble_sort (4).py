e7f9ba42bf830c58b5d1b0a002ac2b7e0373ba77
Irbis174 <matvey.golovkin@mail.ru>
2024-04-03 12:31:48 +0300

    Внесены правки

--- dataset/sort/bubble_sort/bubble_sort (4).py
+++ dataset/sort/bubble_sort/bubble_sort (4).py
@@ -1,9 +1,7 @@
 def bubble_sort(list1):
-    # Outer loop for traverse the entire list
     for i in range(0, len(list1) - 1):
         for j in range(len(list1) - 1):
             if (list1[j] > list1[j + 1]):
-                # here we are not using temp variable
                 list1[j], list1[j + 1] = list1[j + 1], list1[j]
     return list1
 
